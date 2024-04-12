ef0a9e806dbf90b2f6a77363147edc4481f08052
PEDRO ZAVALA <pedropablozavalat@uc.cl>
2024-04-12 02:18:31 -0400

    feat(P2): finish personajes and update index

--- T2-PedroPabloZavalaTejos/main.js
+++ T2-PedroPabloZavalaTejos/main.js
@@ -7,14 +7,14 @@ const SVG1 = d3.select("#vis-1").append("svg");
 const SVG2 = d3.select("#vis-2").append("svg");
 
 // Editar tamaños como estime conveniente
-const WIDTH_VIS_1 = 800;
-const HEIGHT_VIS_1 = 350;
+let WIDTH_VIS_1 = 800;
+let HEIGHT_VIS_1 = 350;
 
-const WIDTH_VIS_2 = 800;
-const HEIGHT_VIS_2 = 1600;
+let WIDTH_VIS_2 = 800;
+let HEIGHT_VIS_2 = 1600;
 
-const HEIGHT = HEIGHT_VIS_1 + HEIGHT_VIS_2;
-const MARGIN = {
+let HEIGHT = HEIGHT_VIS_1 + HEIGHT_VIS_2;
+let MARGIN = {
     top: 90,
     bottom: 90,
     right: 30,
@@ -27,7 +27,26 @@ SVG2.attr("width", WIDTH_VIS_2).attr("height", HEIGHT_VIS_2);
 // Definimos las escalas para poder usarlas en ambas visualizaciones. 
 let escalaManga;
 let escalaCantCaps; 
-let escalaSeries
+let escalaSeries;
+let escalaColorCI;
+let escalaLargoCI;
+let escalaBrazo;
+let escalaAura; 
+let escalaColorAura;
+
+// Definimos un tooltip 
+let tooltip = d3.select("body").append("div")
+    .attr('class', 'tooltip')
+    .style("opacity", 0)
+    .style("width", 300)
+    .style("height", 100)
+    .style("text-align", "left")
+    .style("pointer-events", "none")
+    .style("background", "lightgray")
+    .style("color", 'gray')
+    .style("border-radius", "1rem")
+    .style("padding", "1rem")
+    .style("position", "absolute");
 
 function crearSeries() {
     /* 
@@ -207,6 +226,7 @@ function crearPersonajes(dataset, serie, filtrar_dataset, ordenar_dataset) {
     // Actualizo nombre de un H4 para saber qué hacer con el dataset
     let texto = `Serie: ${serie} - Filtrar: ${filtrar_dataset} - Orden: ${ordenar_dataset}`
     d3.selectAll("#selected").text(texto);
+    
 
     // Nos quedamos con los personajes asociados a la serie seleccionada !
     let datos = dataset.filter(d => {
@@ -220,15 +240,31 @@ function crearPersonajes(dataset, serie, filtrar_dataset, ordenar_dataset) {
             return d.Dragon_ball_gt == true;
         }
     })
-    // Cantidad de personajes por fila 
 
-    const N = 5;
+    // 1. Filtrar, cuando corresponde, por poder_aumenta > 10
+    // Completar aquí
+    if (filtrar_dataset) {
+        datos = datos.filter(d => d.poder_aumenta > 10)
+    }
+    
+    // 2. Ordenar, según corresponda, los personajes. Completar aquí
+    if (ordenar_dataset === 'alfabético') {
+        datos = d3.sort(datos, d => d.personaje)
+    } else if (ordenar_dataset === 'poder_aumenta') {
+        datos = d3.sort(datos, d => d.poder_aumenta)
+    }
+    // En otro caso, se mantiene el por defecto. No se altera el dato original. 
+
+
+    // 3. Confeccionar la visualización 
+
+    let N = 5;
     const RADIO_CABEZA = 10;
     const RADIO_CS = 20; // Radio Cuerpo Superior (CS)
     const ANCHO_BRAZO = 6;
 
     const contenedorPersonajes = SVG2.selectAll('g.personaje')
-        .data(datos)
+        .data(datos, d => d.personaje)
         .join(
             enter => {
                 // Creamos el tag g para cada personaje. 
@@ -242,24 +278,50 @@ function crearPersonajes(dataset, serie, filtrar_dataset, ordenar_dataset) {
                     }
                 );
 
-                personaje.transition('enterPersonaje').duration(500).style('opacity', 1); 
+                personaje.transition('enter-personaje').duration(500).style('opacity', 1);             
                 // ESCALAS
+
                 // Maximos para cada escalas
-                const maxPoderMinimo = d3.max(datos.map(d => d.poder_minimo)); 
-                const maxPoderPromedio = d3.max(datos.map(d => d.poder_promedio)); 
+                let maxPoderMinimo = d3.max(datos.map(d => d.poder_minimo)); 
+                let maxPoderPromedio = d3.max(datos.map(d => d.poder_promedio)); 
+                
                 // Escala desde valores de poder minimo a pixeles entre 5 a 100. 
-                const escalaBrazo = d3.scaleLog().domain([1, maxPoderMinimo]).range([15, 40]);
+                escalaBrazo = d3.scaleLog().domain([1, maxPoderMinimo]).range([15, 40]);
                 // Escala de valores de poder promedio a pixeles entre 5 a 100. CI: Cuerpo inferior
-                const escalaLargoCI = d3.scaleLog().domain([1, maxPoderPromedio]).range([RADIO_CS, 80]); 
+                escalaLargoCI = d3.scaleLog().domain([1, maxPoderPromedio]).range([RADIO_CS, 60]); 
 
                 // Definimos las metricas de aventuras: minimo, maximo y mediana. 
-                const minAventura = d3.min(datos.map(d => d.aventuras)); 
-                const medianAventura= d3.median(datos.map(d => d.aventuras)); 
-                const maxAventura = d3.max(datos.map(d => d.aventuras)); 
+                let minAventura = d3.min(datos.map(d => d.aventuras)); 
+                let medianAventura= d3.median(datos.map(d => d.aventuras)); 
+                let maxAventura = d3.max(datos.map(d => d.aventuras)); 
                 // Definimos una escala divergente para los colores segun las aventuras de c/personaje
-                const escalaColorCI = d3.scaleDiverging(d3.interpolatePiYG)
+                escalaColorCI = d3.scaleDiverging(d3.interpolatePRGn)
                     .domain([minAventura, medianAventura, maxAventura])
                 
+                // [Bonus] Escala para poder_maximo
+                let minPoderMaximo = d3.min(datos.map(d => d.poder_maximo)); 
+                let medianPoderMaximo = d3.mean(datos.map(d => d.poder_maximo)); 
+                let maxPoderMaximo = d3.max(datos.map(d => d.poder_maximo)); 
+                // Escala del tamaño del aura (mas grande, mas poderoso)
+                escalaAura = d3.scaleLog()
+                    .domain([1, maxPoderMaximo])
+                    .range([30, 70]); 
+                // Escala del color, donde mas intenso el color, mas poderoso (poder_maximo alto). 
+                escalaColorAura = d3.scaleLog()
+                    .domain([minPoderMaximo, medianPoderMaximo])
+                    .range(["blue", "yellow"])
+                
+                // [Bonus] Definnimos el maximo poder como un circulo "nominando como aura"
+                // Entre mas poder, el color es mas intenso, entre menos poder, color menos intenso. 
+                
+                personaje.append('circle')
+                    .attr('class', 'aura')
+                    .attr('cx', 0)
+                    .attr('cy', 30)
+                    .attr('opacity', 0.4)
+                    .attr('r', d => escalaAura(d.poder_maximo))
+                    .attr('fill', d => escalaColorAura(d.poder_maximo))
+
                 // Definimos el brazo del personaje. El color sera amarillo para todos. 
                 personaje.append('ellipse')
                     .attr('class', 'brazo')
@@ -274,7 +336,6 @@ function crearPersonajes(dataset, serie, filtrar_dataset, ordenar_dataset) {
                     .attr('class', 'cabeza')
                     .attr('r', RADIO_CABEZA)
                     .attr('fill', d => escalaSeries(d.primera_serie));
-                
                 // Definimos el cuerpo superior. 
                 personaje.append('circle')
                     .attr('class', 'cuerpo-superior')
@@ -301,82 +362,142 @@ function crearPersonajes(dataset, serie, filtrar_dataset, ordenar_dataset) {
                     
                 return personaje;
             },
-            update => update,
-            exit => exit
+            update => {
+                // Actualizamos la posicion
+                update
+                    .transition('update-posicion')
+                    .duration(300)
+                    .attr("transform", (_, i) => {
+                        let posX = (i % N) * 150;
+                        let posY = Math.floor(i / N) * 150;
+                        return `translate(${posX + 100}, ${posY + 100})`;
+                        }
+                    )
+                    .style('opacity', 1)
+                // Actualizamos el cuerpo inferior
+                update.select('.cuerpo-inferior')
+                    .transition('update-cuerpo-inferior')
+                    .duration(500)
+                    .attr('fill', d => escalaColorCI(d.aventuras))
+                    .attr('height', d => escalaLargoCI(d.poder_promedio));
+                // Actualizamos el brazo
+                update.select('.brazo')
+                    .transition('update-brazo')
+                    .duration(500)
+                    .attr('rx', d => escalaBrazo(d.poder_minimo))
+                // Actualizamos el nombre
+                update.select('.nombre')
+                    .transition('update-nombre')
+                    .duration(500)
+                    .text(d => d.personaje)
+                
+                update.select('.aura')
+                    .transition('aura-update')
+                    .duration(500)
+                    .attr('r', d => escalaAura(d.poder_maximo))
+                    .attr('fill', d => escalaColorAura(d.poder_maximo))
+                return update; 
+            },
+            exit => {
+                exit.attr('class', 'delete')
+
+                exit.transition('exit-personaje')
+                    .duration(500)
+                    .attr("transform", (_, i) => {
+                        let posX = 0;
+                        let posY = -120;
+                        return `translate(${posX}, ${posY})`;
+                    })
+                exit.transition("eliminar").style('opacity', 0).duration(200).delay(500).remove();
+                return exit; 
+            }
     );  
 
     // EVENTOS
     // Funcion que resalta un personaje sobre los demas
     const resaltarPersonaje = (personajeActual) => {
         // Opacamos el resto de los personajes
-        console.log(personajeActual)
         contenedorPersonajes
             .filter(d => d.personaje !== personajeActual)
+            .transition('esconder-personaje')
+            .duration(300)
             .style('opacity', 0.1);
         // Resaltamos el personaje con el mouse sobre él 
         contenedorPersonajes
             .filter(d => d.personaje == personajeActual)
+            .transition('resaltar-personaje')
+            .duration(300)
             .style('opacity', 1);
     }
-    contenedorPersonajes.on('mouseover', (_, personajes) => {
+    // evento entrada del mouse
+    contenedorPersonajes.on('mouseenter', (_, personajes) => {
         resaltarPersonaje(personajes.personaje)
     }); 
-    
-    contenedorPersonajes.on('mouseout', (_, personajeActual) => {
+    // evento salida del mouse
+    contenedorPersonajes.on('mouseleave', (_, personajeActual) => {
         // restablecemos la opacidad para cada personaje. 
-        contenedorPersonajes
-            .filter(d => d.personaje !== personajeActual)
+        contenedorPersonajes.filter(d => d.personaje !== personajeActual)
+            .transition('mostrar-personaje')
             .style('opacity', 1);
-
     })
-
-    // 1. Filtrar, cuando corresponde, por poder_aumenta > 10
-    // Completar aquí
-    console.log(filtrar_dataset)
-    
-
-    // 2. Ordenar, según corresponda, los personajes. Completar aquí
-    console.log(ordenar_dataset)
-
-
-    // 3. Confeccionar la visualización 
-    // Todas las escalas deben estar basadas en la información de "datos"
-    // y NO en "dataset".
-
-    console.log(datos)
+    // evento cuando el mouse esta sobre algun personaje
+    contenedorPersonajes.on('mouseover', (e, d) => {
+        const htmlText = `
+            <h3>Datos</h3>
+            <p>Nombre: ${d.personaje}</p>
+            <p>Primera serie: ${d.primera_serie}</p>
+            <p>Serie recurrente: ${d.serie_recurrente}</p>
+            <p>N° Aventuras: ${d.aventuras}</p>
+            <p>N° Poder Aumenta: ${d.poder_aumenta}</p>
+            <p>Poder Máximo: ${d.poder_maximo}</p>
+            <p>Poder Mínimo: ${d.poder_minimo}</p>
+            <p>Poder Promedio: ${d.poder_promedio}</p>
+        `
+        tooltip
+            .html(htmlText)
+            .style("opacity", 1)
+            .style("left", (e.pageX + 10) + "px")
+            .style("top", (e.pageY - 28) + "px");
+    }).on("mouseout", (event, d) => {
+        // cuando el mouse sale del rect, desaparece tooltip
+        tooltip.style("opacity", 0);
+    })
 }
 
+/* 
+Funcion responsive para cuando el ancho de la ventana es menor a 1000px. 
+Altera el tamaño de ambas visualiazciones, achicandolas. 
+Solo se realizaran cambios si el usuario altera el tamaño de la ventana o mueve el mouse. 
+*/ 
+function responsive() {
+    const ajustarVisualizacion = () => {
+        let windowWidth = window.innerWidth;
+        if (windowWidth > 1000) {
+            SVG1.attr('viewBox', '0 70 800 200')
+                .attr('width', WIDTH_VIS_1)
+                .attr('height', HEIGHT_VIS_1);
+        
+            SVG2.attr('viewBox', '-10 10 800 1800')
+                .attr('width', WIDTH_VIS_2)
+                .attr('height', HEIGHT_VIS_2);
+        }
+        else if (windowWidth < 1000) {
+            SVG1.attr('viewBox', '0 -30 800 500')
+                .attr('width', 500)
+                .attr('height', 250)
+            
+            SVG2.attr('viewBox', '0 10 800 1500')
+                .attr('width', 500)
+                .attr('height', 700)  
+        };
+    };
+    window.addEventListener('mousemove', ajustarVisualizacion);
+    window.addEventListener('resize', ajustarVisualizacion);
+}
 
 function ejecutar() {
     crearSeries();
 }
 
 ejecutar();
-
-// /* 
-// Código extra para reproducir música acorde a la tarea.
-// Si no quieres escuchar cuando se carga la página, solo cambia la línea:
-// let playAudio = true; por let playAudio = false;
-// O bien elimina todo el código que está a continuación 😅 
-// */
-// try {
-//     const audio = new Audio('https://github.com/Hernan4444/public_files/raw/main/dbgt.mp3');
-//     audio.volume = 0.3;
-//     audio.loop = true;
-//     let playAudio = false;
-//     if (playAudio) {
-//         audio.play();
-//         d3.select("#sound").text("OFF Music 🎵")
-//     }
-//     d3.select("#sound").on("click", d => {
-//         playAudio = !playAudio;
-//         if (playAudio) {
-//             audio.play();
-//             d3.select("#sound").text("OFF Music 🎵")
-//         }
-//         else {
-//             audio.pause();
-//             d3.select("#sound").text("ON Music 🎵")
-//         }
-//     })
-// } catch (error) { };
+responsive();
